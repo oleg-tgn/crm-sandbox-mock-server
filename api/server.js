ae9cb855174b920e387cda454dd00277ef2d676f
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2025-08-30 18:23:17 +0400

    Get data from json file, save data in memory

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 const jsonServer = require("json-server");
 const path = require("path");
+const fs = require("fs");
 
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
 const dbFile = path.join(__dirname, "db.json");
-const router = jsonServer.router(dbFile);
+const dbData = JSON.parse(fs.readFileSync(dbFile, "utf-8"));
+
+const router = jsonServer.router(dbData);
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
@@ -20,7 +23,7 @@ server.use(
 server.use(router);
 
 server.listen(3000, () => {
-  console.log("JSON Server is running on http://localhost:3000");
+  console.log("JSON Server is running in-memory on http://localhost:3000");
 });
 
 module.exports = server;
